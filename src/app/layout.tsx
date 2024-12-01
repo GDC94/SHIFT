ed9b200048f95ac18eab008c9d9f7d1d3f619a2e
German Derbes Catoni <gderbes@nera-agro.com>
2024-12-01 18:16:06 -0300

    chore: adding new components and client layout wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,10 +20,7 @@ export default function RootLayout({
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📦</text></svg>"
         />
       </head>
-      <body className="h-screen w-screen">
-        <h1>Shift</h1>
-        {children}
-      </body>
+      <body className="h-screen w-screen">{children}</body>
     </html>
   );
 }
--- src/layouts/LandingLayout/LandingLayout.tsx
+++ src/layouts/LandingLayout/LandingLayout.tsx
@@ -1,12 +1,20 @@
 "use client";
 
-import { ClientLayout } from "@/app/components/ClientLayout";
-import { Example } from "@/app/components/CornerNav/CornerNav";
+import { ClientLayout } from "@/components/ClientLayout";
+import Description from "@/components/Description/Description";
+import Section from "@/components/ParallaxSection/ParallaxSection";
+import Hero from "@/sections/Hero/Hero";
+import Nav from "@/sections/Nav/Nav";
 
 const LandingLayout = () => {
   return (
     <ClientLayout>
-      <Example />
+      <main>
+        <Nav />
+        <Hero />
+        <Description />
+        <Section />
+      </main>
     </ClientLayout>
   );
 };
